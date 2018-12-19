4e1bf62cc84e562aa7e5fa0d9e92283e9c7b5e85
zohaibabbasza <zohaibabbasza@outlook.com>
2018-12-19 13:46:35 +0500

    Filter api working

--- foodbear/api/urls.py
+++ foodbear/api/urls.py
@@ -2,9 +2,10 @@
 from django.conf.urls import url
 
 #from .views import AirlineListAPIView,AirlineCreateAPIView,ScheduleListAPIView,SendEmailScheduleAPIView
-from .views import TypeOfFoodsListAPIView,RestaurantsListAPIView
+from .views import TypeOfFoodsListAPIView,RestaurantsListAPIView,ResturantFilterList
 urlpatterns = [
     url('get_type_of_foods/', TypeOfFoodsListAPIView.as_view()),
     url('get_list_of_restaurants/', RestaurantsListAPIView.as_view()),
+    url('get_filter_restaurants/(?P<cat_id>\d+)/', ResturantFilterList.as_view()),
     
 ]
--- foodbear/api/views.py
+++ foodbear/api/views.py
@@ -7,6 +7,8 @@ from rest_framework.authentication import SessionAuthentication,TokenAuthenticat
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from foodbear.models import FoodCategory,Restaurant
+from django.http import HttpResponse,JsonResponse
+
 # from django.views.generic import View
 
 #from foodbear.models import Airline,Schedule
@@ -30,3 +32,10 @@ class RestaurantsListAPIView(APIView):
         qs = Restaurant.objects.all()
         serializer = ResturantSerializer(qs, many=True)
         return Response({'data':serializer.data})
+
+class ResturantFilterList(generics.ListAPIView):
+    serializer_class = ResturantSerializer
+
+    def get_queryset(self):
+        cat_id = self.kwargs['cat_id']
+        return Restaurant.objects.filter(food_category_cat_id=cat_id)
